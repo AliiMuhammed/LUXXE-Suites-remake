df8eccdedc3c7049e8a07b1a1f5a62796b2c14d7
Ali Muhammed <mohamedtaha8000@gmail.com>
2022-09-18 21:05:46 +0200

    Add files via upload

---
+++ js/dark-mood.js
@@ -0,0 +1,18 @@
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    if (document.body.classList.toggle("dark-theme")) {
+        icon.classList = "fa-solid fa-sun"  ;
+    }
+    else {
+    }
+        icon.classList = "fa-solid fa-moon";
+    }
+var icon2 = document.getElementById("icon2");
+icon2.onclick = function () {
+    if (document.body.classList.toggle("dark-theme")) {
+        icon2.classList = "fa-solid fa-sun"  ;
+    }
+    else {
+    }
+        icon2.classList = "fa-solid fa-moon";
+    }
